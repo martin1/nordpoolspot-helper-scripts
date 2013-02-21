e5eb88ddac9c1c5cc9d30bb211fc9564f7e63152
Martin Kurgi <martinkurgi@gmail.com>
2013-02-21 21:38:28 +0200

    "Added code for generating plot for 2011-2012 prices and volumes"

--- data_operations.py
+++ data_operations.py
@@ -1,41 +1,68 @@
-'''
-Created on Feb 18, 2013
+#coding=utf-8
 
-@author: martin
-'''
+import datetime
+from matplotlib.dates import MonthLocator, DateFormatter, DayLocator,\
+    DateLocator, WeekdayLocator, MONDAY
+from matplotlib import pylab
+from matplotlib.pyplot import legend
 
-''''ssh tunnel: ssh -N -p 1521 martin_kurgi@hektor1.ttu.ee -L 9990:127.0.0.1:3306'''
+''''ssh tunnel: ssh -N -p 1521 [username]@[host] -L 9990:127.0.0.1:3306'''
 
 import MySQLdb
 import matplotlib.pyplot as plt
+import matplotlib.dates as d
+from pylab import figure, show
+import numpy
 
-con = MySQLdb.connect(host='127.0.0.1', user = 'martin_kurgi', passwd = '739503', db = 'martin_kurgi', port = 9990)
-cur = con.cursor()  
-cur.execute("select * from buy_price_volume where time = '2011-01-01 00:00:00'")
-buy_rows = cur.fetchall()
-cur.execute("select * from sell_price_volume where time = '2011-01-01 00:00:00'")
-sell_rows = cur.fetchall()
+con = MySQLdb.connect(host='localhost', user = 'root', passwd = '[passwd]', db = 'martin_kurgi', port = 3306)
+cur = con.cursor()
+cur.execute("select distinct date from chart_data where date between '2011-01-01' and '2013-01-01'")
+times = cur.fetchall()
+buy_prices = []
+sell_prices = []
+buy_prices_times = []
+sell_prices_times = []
+times_chart = []  
+for time in times:
+    time_str = datetime.datetime.strftime(time[0], '%Y-%m-%d')
+    print time_str
+    query1 = "select volume from buy_price_volume where date = " + "'"+ time_str + "'"
+    #print query
+    cur.execute(query1)
+    #times_chart.append(time_str.split(' ')[0])
+    buy_prices_times = cur.fetchall()
+    buy_prices.append(numpy.mean(buy_prices_times))
+    
+    query2 = "select volume from sell_price_volume where date = " + "'"+ time_str + "'"
+    cur.execute(query2) 
+    sell_prices_times = cur.fetchall()
+    sell_prices.append(numpy.mean(sell_prices_times))
+    
+for time in times:
+    times_chart.append(d.date2num(time))
+
 con.close()
 
+# every monday
+mondays   = WeekdayLocator(MONDAY)
 
-buy_prices = []
-buy_volumes = []
-sell_prices = []
-sell_volumes = []
+# every 3rd month
+months    = MonthLocator(range(1,13), bymonthday=1, interval=1)
+monthsFmt = DateFormatter("%b '%Y")
 
-for item in buy_rows:
-    buy_prices.append(item[1])
-    buy_volumes.append(item[2])
-    
-for item in sell_rows:
-    sell_prices.append(item[1])
-    sell_volumes.append(item[2])
-    
+fig = figure()
+ax = fig.add_subplot(111)
+ax.plot_date(times_chart, buy_prices, 'b-', label='Volume (buy)')
+ax.plot_date(times_chart, sell_prices, 'r-', label='Volume (sell)')
+ax.xaxis.set_major_locator(months)
+ax.xaxis.set_major_formatter(monthsFmt)
+ax.xaxis.set_minor_locator(mondays)
+ax.autoscale_view()
+#ax.xaxis.grid(False, 'major')
+#ax.xaxis.grid(True, 'minor')
+ax.grid(True)
+legend()
 
-plt.plot(buy_volumes, buy_prices,'b-',sell_volumes, sell_prices, 'r-')
-plt.show()
+fig.autofmt_xdate()
 
-
-#for row in rows:
-#    print row
-#print len(rows)                                                                                                      
+show()                                                                                                   
