227dd2c9a6dd176d9a8742a534afe7b064935ce8
Martin Kurgi <martinkurgi@gmail.com>
2013-03-15 19:10:32 +0200

    Sync

--- plot_dates.py
+++ plot_dates.py
@@ -4,8 +4,10 @@ from matplotlib.dates import date2num, WeekdayLocator, MONDAY, MonthLocator,\
 from matplotlib import pyplot as plt
 from matplotlib.pylab import figure, show
 from scipy.stats.stats import pearsonr
+from scipy.spatial.distance import euclidean
+import csv
 
-start_date = '2011-01-01 00:00:00'
+start_date = '2011-12-01 00:00:00'
 end_date = '2011-12-31 23:00:00'
 
 buy_prices, buy_volumes, times = get_data(start_date, end_date, type='buy')
@@ -35,7 +37,11 @@ sell_volumes_max = list()
 buy_volume_diff = list()
 
 modified_times = list()
+buy_lengths = list()
+sell_lengths = list()
 
+csv_rows_buy = list()
+csv_rows_sell = list()
 
 for i in range(0, len(buy_prices)):
     
@@ -52,6 +58,17 @@ for i in range(0, len(buy_prices)):
     sell_volumes_min.append(min(sell_volumes[i]))
     sell_volumes_max.append(max(sell_volumes[i]))
     
+    #Line lengths
+    length = 0
+    for j in range(0, len(buy_volumes[i])-1):
+        length += euclidean([buy_volumes[i][j], buy_prices[i][j]],[buy_volumes[i][j+1], buy_prices[i][j+1]])
+    buy_lengths.append(length)
+    
+    '''length = 0
+    for j in range(0, len(sell_volumes[i])-1):
+        length += euclidean([sell_volumes[i][j], sell_prices[i][j]],[sell_volumes[i][j+1], sell_prices[i][j+1]])
+    sell_lengths.append(length)'''
+    
     #buy_volume_diff.append(max(buy_volumes[i]) - min(buy_volumes[i]))
     
     '''if intersect_y > 120:
@@ -62,9 +79,33 @@ for i in range(0, len(buy_prices)):
     volumes.append(intersect_x)
     prices.append(intersect_y)
 
+    csv_rows_buy.append([str(times[i][0]), min(buy_volumes[i]), max(buy_volumes[i]), max(buy_volumes[i])-min(buy_volumes[i]), length, intersect_x[0], intersect_y[0]])
+    
+    length = 0
+    for j in range(0, len(buy_volumes[i])-1):
+        length += euclidean([sell_volumes[i][j], sell_prices[i][j]],[sell_volumes[i][j+1], sell_prices[i][j+1]])
+    sell_lengths.append(length)
+    
+    csv_rows_sell.append([str(times[i][0]), min(sell_volumes[i]), max(sell_volumes[i]), max(sell_volumes[i])-min(sell_volumes[i]), length, intersect_x[0], intersect_y[0]])
 
+#write data to csv
+with open("2011_buy_curve_data.csv", 'ab') as csvfile:
+        wr = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        wr.writerow(["time", "min_volume", "max_volume", "volume_difference", "curve_length", "system_price", "system_volume"])
+        for item in csv_rows_buy:
+            wr.writerow(item)
+        csvfile.close()
+        
+with open("2011_sell_curve_data.csv", 'ab') as csvfile:
+        wr = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        wr.writerow(["time", "min_volume", "max_volume", "volume_difference", "curve_length", "system_price", "system_volume"])
+        for item in csv_rows_sell:
+            wr.writerow(item)
+        csvfile.close()
 
-times_num = [date2num(t) for t in times]  
+    
+
+'''times_num = [date2num(t) for t in times]  
 modified_times = [date2num(t) for t in modified_times]
 modified_y = [50 for t in modified_times]  
 fig = figure()
@@ -78,19 +119,20 @@ fig = figure()
 ax = fig.add_subplot(111)
 #ax2 = fig.add_subplot(211)
 #ax.plot_date(times, buy_volume_diff, 'b-')
-ax.plot_date(times, sell_volumes_min, 'r-')
-ax.plot_date(times_num[sell_volumes_min.index(max(sell_volumes_min))], max(sell_volumes_min), 'bo')
-ax.plot_date(times_num[sell_volumes_min.index(min(sell_volumes_min))], min(sell_volumes_min), 'bo')
-ax.annotate(str(times[sell_volumes_min.index(max(sell_volumes_min))][0]) +','+ str(max(sell_volumes_min)), xy=(times_num[sell_volumes_min.index(max(sell_volumes_min))], max(sell_volumes_min)), 
-            xytext=(times_num[sell_volumes_min.index(max(sell_volumes_min))], max(sell_volumes_min)+200))
-ax.annotate(str(times[sell_volumes_min.index(min(sell_volumes_min))][0]) +','+ str(min(sell_volumes_min)), xy=(times_num[sell_volumes_min.index(min(sell_volumes_min))], min(sell_volumes_min)), 
-            xytext=(times_num[sell_volumes_min.index(min(sell_volumes_min))], min(sell_volumes_min)-200))
-ax.set_title("2011 hourly min sell volume")
+ax.plot(times, buy_lengths, 'r-')
+#ax.plot_date(times, sell_volumes_min, 'r-')
+ax.plot_date(times_num[buy_lengths.index(max(buy_lengths))], max(buy_lengths), 'bo')
+ax.plot_date(times_num[buy_lengths.index(min(buy_lengths))], min(buy_lengths), 'bo')
+ax.annotate(str(times[buy_lengths.index(max(buy_lengths))][0]) +','+ str(max(buy_lengths)), xy=(times_num[buy_lengths.index(max(buy_lengths))], max(buy_lengths)), 
+            xytext=(times_num[buy_lengths.index(max(buy_lengths))], max(buy_lengths)+200))
+ax.annotate(str(times[buy_lengths.index(min(buy_lengths))][0]) +','+ str(min(buy_lengths)), xy=(times_num[buy_lengths.index(min(buy_lengths))], min(buy_lengths)), 
+            xytext=(times_num[buy_lengths.index(min(buy_lengths))], min(buy_lengths)-200))
+ax.set_title("2011 hourly sell curve lengths")
 #print str(times[buy_volumes_max.index(max(buy_volumes_max))][0]), max(buy_volumes_max)
 #print str(times[buy_volumes_max.index(min(buy_volumes_max))][0]), min(buy_volumes_max)
 #print pearsonr(buy_volumes_min, buy_volumes_max)
 #ax2.plot_date(times, volumes, 'r-')
-#ax.xaxis.set_major_locator(months)'''
+#ax.xaxis.set_major_locator(months)
 #axarr[0].xaxis.set_major_locator(days)
 #axarr[0].xaxis.set_major_formatter(daysFmt)
 
@@ -122,3 +164,4 @@ ax.grid(True)
 
 fig.autofmt_xdate()
 show()
+'''
